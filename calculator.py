c72f8d552e01b85cb5fa5a0922564155ddce27b4
Sergi Navarro <sergi.navarro@gmail.com>
2019-07-21 13:00:14 +0100

    Add functions for addition, substraction, multiply and divide

---
+++ calculator.py
@@ -0,0 +1,17 @@
+"""
+
+Calculator library 
+
+"""
+
+def add(n1, n2):
+	return n1 + n2
+
+def substact(n1, n2):
+	return n1 - n2
+
+def multpiply(n1, n2):
+	return n1 * n2
+
+def substact(n1, n2):
+	return n1 / n2
